0a6a07f28070abe0940e7d82f6f77e9f37de3849
Ernesto Pedrosa Alonso <ernestop891123@gmail.com>
2024-07-11 15:16:00 +0200

    feat: update settings with some default values

--- dog-app-server/src/types/interfaces/settings.ts
+++ dog-app-server/src/types/interfaces/settings.ts
@@ -1,6 +1,8 @@
 export const Settings = {
-	apiVersion: process.env.API_VERSION,
-	port: process.env.PORT,
-	defaultPaginationSize: process.env.DEFAULT_PAGINATION_SIZE,
-	cacheExpiresAfterMinutes: process.env.CACHE_EXPIRES_AFTER_MINUTES
+	apiVersion: process.env.API_VERSION ?? 'v1',
+	port: parseInt(process.env.PORT ?? '300'),
+	defaultPaginationSize: parseInt(process.env.DEFAULT_PAGINATION_SIZE ?? '10'),
+	cacheExpiresAfterMinutes: parseInt(process.env.CACHE_EXPIRES_AFTER_MINUTES ?? '10'),
+	cacheLimit: parseInt(process.env.CACHE_LIMIT ?? '200'),
+	cacheCleaningFrequency: parseInt(process.env.CACHE_CLEANING_FREQUENCY ?? '1000')
 }
